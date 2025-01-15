0f2c776abc23a8767fef9a257b14d2ee71b95e89
Judah Valerie <90010677+judah-v@users.noreply.github.com>
2025-01-14 20:52:09 -0500

    split lines

--- data/app.py
+++ data/app.py
@@ -1,41 +1,27 @@
-from dash import Dash, html, dcc
+from dash import Dash, html, dcc, Input, Output, callback
 import plotly.graph_objs as go
 import pandas as pd
 
 df = pd.read_csv('quantium-starter-repo\\data\\output_file.csv')
 
+regions = {
+    'North': [df[df['region'] == 'north'], 'green'],
+    'South': [df[df['region'] == 'south'], 'red'],
+    'East': [df[df['region'] == 'east'], 'orange'],
+    'West': [df[df['region'] == 'west'], 'purple'],
+    'All': [df, 'blue'],
+}
 
-north = df[df['region'] == 'north']
-south = df[df['region'] == 'south']
-east = df[df['region'] == 'east']
-west = df[df['region'] == 'west']
-
-app = Dash()
 graph = dcc.Graph(
-    'line-graph',
+    id='line-graph',
     figure={
         'data': [
             go.Scatter(
-                x = north['date'],
-                y = north['sales'],
+                x = df['date'],
+                y = df['sales'],
                 mode = 'lines',
-                name = 'North Region'
-            ), go.Scatter(
-                x = south['date'],
-                y = south['sales'],
-                mode = 'lines',
-                name = 'South Region'
-            ), go.Scatter(
-                x = east['date'],
-                y = east['sales'],
-                mode = 'lines',
-                name = 'East Region'
-            ), go.Scatter(
-                x = west['date'],
-                y = west['sales'],
-                mode = 'lines',
-                name = 'West Region'
-            ), 
+                name = 'Sales'
+            )
         ],
         'layout':{
             'title':'Sales before and after price change'
@@ -43,9 +29,34 @@ graph = dcc.Graph(
     }
 )
 
+app = Dash()
 app.layout = [
+    dcc.RadioItems(id='radio', options=['North', 'South', 'East', 'West', 'All'], value='All'),
     graph,
 ]
+
+@callback(
+    Output(component_id='line-graph', component_property='figure'),
+    Input(component_id='radio', component_property='value')
+)
+def get_line(region):
+    region_df = regions[region]
+    figure={
+        'data': [
+            go.Scatter(
+                x = region_df[0]['date'],
+                y = region_df[0]['sales'],
+                mode = 'lines',
+                name = region + ' Region',
+                line = {'color': region_df[1]}
+            )
+        ],
+        'layout':{
+            'title':'Sales before and after price change'
+        }
+    }
+    return figure 
+
 
 if __name__ == '__main__':
     app.run(debug=True)
